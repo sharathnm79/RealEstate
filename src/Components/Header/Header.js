79faddc795c651f755399af0d4588e893a05a6ea
sharathnm79 <105986026+sharathnm79@users.noreply.github.com>
2024-01-07 23:43:49 +0530

    Update Header.js
    updated '/'

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -22,10 +22,10 @@ function Header() {
           }}
         >
           <div className=" flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-            <a href="">Recidencies</a>
-            <a href="">Our Value</a>
-            <a href="">Contact Us</a>
-            <a href="">Get Started</a>
+            <a href="/">Recidencies</a>
+            <a href="/">Our Value</a>
+            <a href="/">Contact Us</a>
+            <a href="/">Get Started</a>
             <button className="button">Contact </button>
           </div>
         </OutsideClickHandler>
